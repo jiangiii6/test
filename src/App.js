945357cc38218a8f77ef00dfbf39d51cda52621e
Jiang <38676115+jiangiii6@users.noreply.github.com>
2022-06-14 13:08:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// export default function App() {
+//   return <h1>Hello World</h1>;
+// }
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+      <h1>Hello World</h1> 
+      </div>
+    );
+  }
+}
+
+export default App;
